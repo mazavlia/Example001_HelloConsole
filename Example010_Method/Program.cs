6ecacf04b1bf1161cb369f5da01dc79d47693450
marina zaporozhets <mazavlia@mail.ru>
2022-09-03 21:28:36 +0500

    end Example010_Method

--- Example010_Method/Program.cs
+++ Example010_Method/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = { 1, 2, 28, 33, 45, 57, 17, 28, 39 };
+
+int n = array.Length;
+int find = 28;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        
+        //прерывание цикла. В массиве 2 значения "28", нам нужно, чтобы цикл прервался после нахождения первого значения
+        break;
+    }
+    index++;
+
+}
