f6d48fc19f8d35c3a47f3d04d05f711e5985f985
marina zaporozhets <mazavlia@mail.ru>
2022-09-03 23:52:24 +0500

    add example011

---
+++ Example011_Array/Program.cs
@@ -0,0 +1,56 @@
+﻿void FillArray(int[] collection)
+
+{
+    int Length = collection.Length;
+    int index = 0;
+    while (index < Length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int indexOf(int[] collection, int find) 
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;            
+        }
+        index++;
+    }
+    return position;
+}
+
+// Создаем новый массив, в котором будет 10 элементов
+int[] array = new int[10];
+
+// Заполним наш массив.
+FillArray(array);
+// После автоматического заполнения массива можно вручную назначить определенным элементам массива нужные значения.
+array[2] = 4;
+array[8] = 4;
+
+//Распечатаем массив.
+PrintArray(array);
+
+Console.WriteLine();
+int pos = indexOf(array, 444);
+Console.WriteLine(pos);
